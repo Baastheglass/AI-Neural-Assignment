1b8d6241f6f4f0c63071d94a3e0e12283cbb9461
Baastheglass <lightningblazer123@gmail.com>
2025-03-24 16:26:51 +0500

    i and ii done

---
+++ main.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow import keras
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data() 
+
+# Normalizing the dataset 
+x_train = x_train/255
+x_test = x_test/255
+  
+# Flatting the dataset in order 
+# to compute for model building 
+x_train_flatten = x_train.reshape(len(x_train), 28*28) 
+x_test_flatten = x_test.reshape(len(x_test), 28*28) 
+
+#i)
+slpmodel = keras.Sequential([keras.layers.Dense(10, input_shape=(784,), activation='sigmoid')]) 
+slpmodel.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy']) 
+slpmodel.fit(x_train_flatten, y_train, epochs=5) 
+print("Evaluating Single Layer Perceptron: ")
+slpmodel.evaluate(x_test_flatten, y_test) 
+
+#ii)
+mlpmodel = keras.Sequential([keras.layers.Dense(128, input_shape=(784,), activation='relu'),  # Hidden layer 1
+    keras.layers.Dense(64, activation='relu'),                     # Hidden layer 2
+    keras.layers.Dense(10, activation='softmax')                   # Output layer
+])
+
+mlpmodel.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+mlpmodel.fit(x_train_flatten, y_train, epochs=5)
+print("Evaluating Multi Layer Perceptron: ")
+mlpmodel.evaluate(x_test_flatten, y_test)
+
